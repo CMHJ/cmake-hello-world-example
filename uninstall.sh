ee7305137f7c8ca9d4401e7dce63aacc17657718
Connor Jarvis <connor.jarvis@defendtex.com>
2022-06-01 16:38:34 +1000

    updated uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-current_dir=$(dirname $0)
-manifest_dir="build/install_manifest.txt"
-echo "Using $current_dir/$manifest_dir"
+# Check if build directory exists
+! [ -d "./build" ] && \
+    echo "Error: No \"build\" dir found" && \
+    echo "Cannot find install_manifest.txt" && \
+    exit 1
+
+dir=$(dirname "$0")
+manifest_path="build/install_manifest.txt"
+
+echo "Using $dir/$manifest_path"
 echo "Uninstalling files:"
-cat $current_dir/$manifest_dir
-echo
-xargs rm < $current_dir/$manifest_dir
+cat "$dir/$manifest_path"
+echo # echo newline
+
+# Perform uninstall of files
+xargs rm < "$dir/$manifest_path"
+
